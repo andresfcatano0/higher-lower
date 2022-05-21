5f422b317ef4fee919c4344d608f5a49af9e137d
Andres Catano <andresfcatano0@gmail.com>
2022-05-20 20:15:10 -0400

    Add button to restart the game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let guess = document.getElementById("guess");
 let button = document.getElementById("button");
+let button2 = document.getElementById("button2");
 let message = document.getElementById("message");
 let userInput = document.getElementById("userInput");
 let valid = false;
@@ -7,17 +8,21 @@ let random;
 let maxNum;
 let userGuess = [];
 
-while (valid === false) {
-	maxNum = Math.round(Number(prompt("Choose a maximum number")));
-	userInput.innerHTML = (maxNum);
-	if (maxNum !== NaN && maxNum > 0){
-		valid = true;
-		random = Math.floor(Math.random() * maxNum) + 1;
+function initialCheck(){
+	while (valid === false) {
+		maxNum = Math.round(Number(prompt("Choose a maximum number")));
+		userInput.innerHTML = (maxNum);
+		if (maxNum !== NaN && maxNum > 0){
+			valid = true;
+			random = Math.floor(Math.random() * maxNum) + 1;
+		}
 	}
 }
 
 function inRange(){
-	if (guess.value < 1){
+	if (guess.value == ""){
+		//pass
+	} else if (guess.value < 1){
 		alert("That number is not in range, try again. (The guess you entered is less than 1)");
 	} else if (guess.value > maxNum){
 		alert("That number is not in range, try again. (The guess you entered is greater than the maximum number)");
@@ -45,12 +50,28 @@ button.addEventListener("click", function(){
 	if (num == random){
 		check();
 		message.innerHTML = ("You got it! it took you " + userGuess.length + " tries and your guesses were " + userGuess.join(", "));
+		button.disabled = true;
+		button2.style.display = "inline"; 
+	} else if (guess.value == ""){
+		message.innerHTML = ('<p class="animate__animated animate__headShake">Please Enter a valid number</p>');
 	} else if (num < random){
-		message.innerHTML = ("No, try a higher number");
+		message.innerHTML = ('<p class="animate__animated animate__headShake">No, try a higher number</p>');
 		check();
 	} else if (num > random){
-		message.innerHTML = ("No, try a lower number");
+		message.innerHTML = ('<p class="animate__animated animate__headShake">No, try a lower number</p>');
 		check();
 	}
 })
+
+button2.addEventListener("click", function(){
+	valid = false;
+	userGuess = [];
+	button.disabled = false;
+	button2.style.display = "none"; 
+	guess.value = " ";
+	message.innerHTML = "";
+	initialCheck()
+})
+
+initialCheck()
 
